db77841c154aa594b5a2b71791686f25725b8a19
badiyanto77 <badiyanto@gmail.com>
2025-06-27 19:45:56 -0400

    add files

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# Use AWS Lambda Python 3.9 base image for x86_64 (not arm64)
+# FROM --platform=linux/amd64 public.ecr.aws/lambda/python:3.9
+# Base Python image
+# FROM public.ecr.aws/lambda/python:3.11
+FROM public.ecr.aws/lambda/python:3.12
+
+
+# Set working directory to /var/task (AWS Lambda default)
+WORKDIR /var/task
+
+# # Copy application files into the container
+# COPY lambda_function.py .
+# COPY requirements.txt . 
+
+# Copy requirements.txt
+COPY requirements.txt ${LAMBDA_TASK_ROOT}
+
+# Install dependencies
+# RUN pip install --no-cache-dir -r requirements.txt
+RUN pip install -r requirements.txt
+
+# Copy function code
+COPY lambda_function.py ${LAMBDA_TASK_ROOT}
+
+# Set the entry point and handler correctly
+# ENTRYPOINT ["/var/lang/bin/python3.12", "-m", "awslambdaric"]
+
+# ENTRYPOINT ["/lambda-entrypoint.sh"]
+# CMD ["lambda_function.lambda_handler"]
+CMD ["lambda_function.handler"]
---
+++ commands.sh
@@ -0,0 +1,26 @@
+## command to create a repository in AWS ECR With name calendly
+
+aws ecr create-repository --repository-name calendly
+
+## command  to buld the Docker Container Image (dont forget the dot at the end)
+docker build -t calendly .
+
+docker build --platform linux/amd64 -t calendly .
+
+
+## Command to give the tag Lastest to the Docker Image
+
+docker tag calendly:latest 891662393358.dkr.ecr.us-east-2.amazonaws.com/calendly:latest
+
+##you can get above URI from AWS ECR console
+## Command to connect to AWS ECR
+## aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 891662393358.dkr.ecr.us-east-2.amazonaws.com
+
+aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 891662393358.dkr.ecr.us-east-2.amazonaws.com
+
+## replace this 980921724429.dkr.ecr.us-east-1.amazonaws.com with your URI from AWS ECR console
+## Command to Push the latest Image to AWS ECR
+
+docker push 891662393358.dkr.ecr.us-east-2.amazonaws.com/calendly:latest
+
+
